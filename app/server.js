6e95c5452f105d4832f3fc50982d193a3c6375ee
Denis Savosin <dsavosin@at-consulting.ru>
2019-02-23 15:29:55 +0300

    Removed http server, builded from node:8 docker image

--- app/server.js
+++ app/server.js
@@ -1,38 +1,16 @@
 // Source: https://github.com/einaros/ws
- 
+
 var WebSocketServer = require('ws').Server
 , wss = new WebSocketServer({ port: 8080 });
- 
-wss.on('connection', function connection(ws) 
+
+wss.on('connection', function connection(ws)
 {
-   ws.on('message', function incoming(message) 
+   ws.on('message', function incoming(message)
    {
       console.log('received: %s', message);
    });
- 
+
    ws.send('something');
-}); 
+});
 
-
-// static webserver
-// Source: https://www.npmjs.com/package/node-static
-
-var static = require('node-static');
- 
-var fileServer = new static.Server('/root');
- 
-require('http').createServer(function (request, response) 
-{
-    request.addListener('end', function () 
-    {
-        fileServer.serve(request, response);
-    }).resume();
-}).listen(80);
-
-
-console.log('Listening on port 80.');
-
-
-
-
-
+console.log('Listening on port 8080.');
